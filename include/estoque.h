ec85f192a17c080dbcf90d020484ab0fd9c9378d
Eduardo Abrantes <eduardopabrantes@gmail.com>
2024-09-18 09:08:51 -0300

    feature: criação da estrutura de avoree avl de estoque

--- include/estoque.h
+++ include/estoque.h
@@ -12,3 +12,8 @@ typedef struct Item Item;
  * @brief Estrutura para representar um cliente.
  */
 typedef struct NodoAVL NodoAVL;
+
+NodoAVL* criarNodoAVL(Item item);
+NodoAVL* rotacaoDireita(NodoAVL* y);
+NodoAVL* rotacaoEsquerda(NodoAVL* x);
+int obterBalanceamento(NodoAVL* nodo);
--- src/estoque.c
+++ src/estoque.c
@@ -22,3 +22,43 @@ int altura(NodoAVL* nodo) {
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
+
+NodoAVL* criarNodoAVL(Item item) {
+    NodoAVL* nodo = (NodoAVL*)malloc(sizeof(NodoAVL));
+    nodo->item = item;
+    nodo->esquerda = nodo->direita = NULL;
+    nodo->altura = 1;
+    return nodo;
+}
+
+NodoAVL* rotacaoDireita(NodoAVL* y) {
+    NodoAVL* x = y->esquerda;
+    NodoAVL* T2 = x->direita;
+
+    x->direita = y;
+    y->esquerda = T2;
+
+    y->altura = max(altura(y->esquerda), altura(y->direita)) + 1;
+    x->altura = max(altura(x->esquerda), altura(x->direita)) + 1;
+
+    return x;
+}
+
+NodoAVL* rotacaoEsquerda(NodoAVL* x) {
+    NodoAVL* y = x->direita;
+    NodoAVL* T2 = y->esquerda;
+
+    y->esquerda = x;
+    x->direita = T2;
+
+    x->altura = max(altura(x->esquerda), altura(x->direita)) + 1;
+    y->altura = max(altura(y->esquerda), altura(y->direita)) + 1;
+
+    return y;
+}
+
+int obterBalanceamento(NodoAVL* nodo) {
+    if (nodo == NULL)
+        return 0;
+    return altura(nodo->esquerda) - altura(nodo->direita);
+}
